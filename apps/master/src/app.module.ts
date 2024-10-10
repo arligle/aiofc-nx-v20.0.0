f5640418aa6737b0a9360b5c09468ea9f3646834
arligle <0774543002@163.com>
2024-10-10 17:23:10 +0800

    feat(apps): master integrated @aiokit/typeorm and nestjs-typeorm

--- apps/master/src/app.module.ts
+++ apps/master/src/app.module.ts
@@ -4,15 +4,18 @@ import { AppController } from './app/app.controller';
 import { AppService } from './app/app.service';
 import RootConfig from './config/root.config';
 import { setupYamlBaseConfigModule } from '@aiokit/config';
-// import { LoggerModule } from 'nestjs-pino';
 import { setupLoggerModule } from '@aiokit/logger';
 import { Logger } from 'nestjs-pino';
+import { setupTypeormModule } from '@aiokit/typeorm';
+import * as Entities from './database/entities';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     setupYamlBaseConfigModule(__dirname, RootConfig),
-    // LoggerModule.forRoot()
     setupLoggerModule(),
+    TypeOrmModule.forFeature(Object.values(Entities)),
+    setupTypeormModule()
   ],
   controllers: [AppController],
   providers: [AppService, Logger],
